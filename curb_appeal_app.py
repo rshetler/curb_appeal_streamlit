5e0d756dd0c78e8e9790ef6f9bf3930f4a1c2ea6
rshetler <rodney.shetler@gmail.com>
2023-12-13 15:45:54 -0500

    fixed rotation

--- curb_appeal_app.py
+++ curb_appeal_app.py
@@ -79,7 +79,8 @@ def imagen_request(image_path: str, upload,rand_seed,strength):
         # Do nothing
         print("No rotation to perform based on Exif data")
     #Display uploaded image with correct rotation
-    col1.image(input_img)
+    col1.write("#### Original Image:")
+    col1.image(input_img, width=400)
 
     buffer = BytesIO()
     input_img.save(buffer, format="png")
@@ -169,7 +170,7 @@ def main():
 
         with container2:
             if my_upload is not None:
-                col1.write("### Original Image:")
+                #col1.write("### Original Image:")
                 #col1.image(my_upload)
         with container3:
                 if generate_button:
